26b0fc1fa5d4c298cfd4dc8e93997f9a59b8b548
Feben Getachew <febengetachew580@gmail.com>
2025-10-27 12:03:39 +0300

    banner

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Banner from './components/Banner/Banner'
 
 function App() {
  
@@ -10,6 +11,7 @@ function App() {
 
    <>
     <Header />
+    <Banner />
     <Footer />
    </>
   )
